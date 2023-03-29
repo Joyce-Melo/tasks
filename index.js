74adb7151d69f07f85b20316beb2ffeb3a33c241
Joyce Melo <joyce.santosm@hotmail.com>
2023-03-29 13:06:06 -0300

    Refactor: Comments removed

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-import express from 'express';
-import tasksRouter from './src/routes/tasks.routes.js'
+import express from "express";
+import tasksRouter from "./src/routes/tasks.routes.js";
 
 const app = express();
 const port = 4004;
 
-app.use(express.json())
+app.use(express.json());
 
-app.use('/tasks', tasksRouter)
-app.use('/tasks', tasksRouter)
-
-
+app.use("/tasks", tasksRouter);
 
 app.listen(port, () => console.log(`Servidor rodando na porta ${port}`));
--- src/dbconfig/config.js
+++ src/dbconfig/config.js
@@ -1,8 +1,4 @@
-//const sqlite3 = require("sqlite3")
-//const { open } = require ("sqlite");
-
 import sqlite3 from "sqlite3";
-//const db = new sqlite3.Database(':memory');
 import { open } from "sqlite";
 
 export default  async () =>
